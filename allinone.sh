9788f84fee8dabb24653bffb7d4a62c8f4e68796
liwenjie <liwenjie119@126.com>
2020-12-18 15:32:24 +0800

    fix wget

--- allinone.sh
+++ allinone.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 wget https://git.io/v2ray.sh
-get https://raw.githubusercontent.com/liwenjie119/centos7-sshd/master/v2autoinstall.sh
+wget https://raw.githubusercontent.com/liwenjie119/centos7-sshd/master/v2autoinstall.sh
 wget https://raw.githubusercontent.com/liwenjie119/centos7-sshd/master/builds.sh
 wget https://raw.githubusercontent.com/liwenjie119/centos7-sshd/master/tinyautoinstall.sh
 chmod 777 *.sh
